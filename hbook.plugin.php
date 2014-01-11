6b04cc64b42ae87a63f6491444ab03f8952e353d
Morgante Pell <morgante.pell@morgante.net>
2014-01-12 01:04:34 +0400

    awesome shit

--- hbook.plugin.php
+++ hbook.plugin.php
@@ -6,7 +6,16 @@ if ( !defined( 'HABARI_PATH' ) ) {
 
 class Hbook extends Plugin
 {
+	public function configure()
+	{
+		$ui = new FormUI( 'Hbook' );
 
+		$ui->append('text', 'fb_app_id', 'option:hbook__fb_app_id', _t('Facebook App ID', 'hbook'));
+		$ui->append('text', 'fb_app_secret', 'option:hbook__fb_app_secret', _t('Facebook App Secret', 'hbook'));
+
+		$ui->append( 'submit', 'save', _t( 'Save' ) );
+		$ui->out();
+	}
 }
 
 ?>
