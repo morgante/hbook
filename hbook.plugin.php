d6622abd14f14e554b4156fe117e6921a4132d09
Morgante Pell <morgante.pell@morgante.net>
2014-01-12 02:30:58 +0400

    start screwing around with Facebook login via SDK... but it's being buggy

--- hbook.plugin.php
+++ hbook.plugin.php
@@ -4,8 +4,62 @@ if ( !defined( 'HABARI_PATH' ) ) {
 	die( 'No direct access' );
 }
 
+require_once('facebook-sdk/src/facebook.php');
+
 class Hbook extends Plugin
 {
+	private $facebook;
+
+	public function action_init()
+	{
+		if ($this->is_ready()) {
+			$this->facebook = new Facebook(array(
+				'appId' => Options::get('hbook__fb_app_id'),
+				'secret' => Options::get('hbook__fb_app_secret'),
+				'cookie' => true,
+				'code' => $_REQUEST['code']
+			));
+
+			$this->add_rule('"auth"/"login"/"facebook"', 'login_facebook');
+		}
+	}
+
+	public function action_plugin_act_login_facebook()
+	{
+		Utils::debug($_REQUEST);
+		Utils::debug($this->facebook->getAccessToken());
+		$this->facebook->setAccessToken($this->facebook->getAccessToken());
+		Utils::debug($this->facebook->api('/me'));
+		exit;
+	}
+
+	private function is_ready()
+	{
+		if (Options::get('hbook__fb_app_id') && Options::get('hbook__fb_app_secret')) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private function get_login_url()
+	{
+		$params = array(
+			// 'scope' => 'read_stream, friends_likes',
+			// 'redirect_uri' => URL::get('login_facebook')
+		);
+
+		return $this->facebook->getLoginUrl();
+	}
+
+	public function action_theme_loginform_controls()
+	{
+		echo '<a href="' . $this->get_login_url() . '">Log in with Facebook</a>';
+				Utils::debug($_REQUEST);
+
+		Utils::debug($this->facebook->getUser());
+	}
+
 	/**
 	 * Build main plugin configuration form
 	 * @return void
@@ -32,7 +86,7 @@ class Hbook extends Plugin
     	$form->user_info->facebook_id->class[] = 'clear';
             
     }
-    
+
 }
 
 ?>
