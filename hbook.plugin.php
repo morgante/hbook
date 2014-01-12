ceca4a86adce02b44a55b978962a0203afe3eec8
Morgante Pell <morgante.pell@morgante.net>
2014-01-12 23:57:35 +0400

    cleanup

--- hbook.plugin.php
+++ hbook.plugin.php
@@ -6,29 +6,18 @@ if ( !defined( 'HABARI_PATH' ) ) {
 	die( 'No direct access' );
 }
 
-require_once('facebook-sdk/src/facebook.php');
-use Facebook;
-
 class Hbook extends Plugin
 {
-	private $facebook;
-	private $service = 'facebook';
+	private $service = 'Facebook';
 
+	/**
+	 * If we're ready, add our rewrite rules
+	 */
 	public function action_init()
 	{
 		if ($this->is_ready()) {
-			// $this->facebook = new Facebook(array(
-				// 'appId' => Options::get('hbook__fb_app_id'),
-				// 'secret' => Options::get('hbook__fb_app_secret'),
-				// 'cookie' => true,
-			// ));
-
 			$this->add_rule('"auth"/"facebook"/"add"', 'add_facebook_user');
 			$this->add_rule('"auth"/"facebook"/"callback"', 'facebook_oauth_callback');
-
-			// $this->check_user();
-
-			// Stack::add( 'admin_header_javascript', URL::get_from_filesystem(__FILE__) . '/admin.js', 'hbook-admin', array('jquery') );
 		}
 	}
 
@@ -55,70 +44,10 @@ class Hbook extends Plugin
 		}
     }
 
-    private function build_link($type, $params)
-    {
-    	switch ($type) {
-    		case 'oauth':
-    			$url = "https://www.facebook.com/dialog/oauth?";
-    			break;
-    		case 'token':
-    			$url = 'https://graph.facebook.com/oauth/access_token?';
-    			break;
-    	}
-
-    	$url .= 'client_id=' . Options::get('hbook__fb_app_id');
-
-    	if ($type == 'token') {
-    		$url .= '&client_secret=' . Options::get('hbook__fb_app_secret');
-    		$url .= '&code=' . $params['code'];
-    	}
-
-    	if ($type == 'oauth') {
-    		if (isset($params['scope'])) {
-	    		$scopes = $params['scope'];
-	    	} else {
-	    		$scopes = Options::get('hbook__fb_scopes');
-	    	}
-
-	    	if ($scopes !== null) {
-	    		if (is_string($scopes)) {
-	    			$scopes = str_replace(' ', '', $scopes);
-	    			$scopes = explode(',', $scopes);
-	    		}
-	    		$scopes = implode(',', $scopes);
-	    		$url.= '&scopes=' . $scopes;
-	    	}
-    	}
-
-		$url .= "&redirect_uri=" . URL::get('facebook_oauth_callback');
-
-		if(isset($params['state'])) {
-			$url .= "&state=" . $params['state'];
-		}
-
-		return $url;
-    }
-
-    private function api( $path, $token )
-    {
-    	$base = 'https://graph.facebook.com';
-
-    	$request = new RemoteRequest($base . $path . '?access_token=' . $token , "GET");
-		$request->add_header('Accept: application/json');
-		$request->execute();
-
-		$response = $request->get_response_body();
-		$response = json_decode($response);
-
-		return $response;
-
-    }
-
-    private function get_portrait_url($user_id)
-    {
-    	return 'https://graph.facebook.com/' . $user_id . '/picture';
-    }
-
+    /**
+     * Handle the oauth callback Facebook directs us to and forward it to socialauth
+     * @return void
+     */
     public function action_plugin_act_facebook_oauth_callback($handler)
     {
     	$code = $_GET['code'];
@@ -168,6 +97,10 @@ class Hbook extends Plugin
 		}
     }
 
+    /**
+     * Handle adding a new user via their Facebook ID
+     * @return void
+     */
 	public function action_plugin_act_add_facebook_user()
 	{
 		if (!User::identify()->can( 'manage_users' )) {
@@ -227,31 +160,25 @@ class Hbook extends Plugin
 		}
 	}
 
-	public function check_user()
+	/**
+	 * Build main plugin configuration form
+	 * @return void
+	 */
+	public function configure()
 	{
-		// No sense in trying if we're already logged in
-		if (User::identify()->loggedin) {
-			return;
-		}
+		$ui = new FormUI( 'Hbook' );
 
-		if ($this->facebook->getUser()) {
+		$ui->append('text', 'fb_app_id', 'option:hbook__fb_app_id')->label( _t('Facebook App ID', 'hbook') );
+		$ui->append('text', 'fb_app_secret', 'option:hbook__fb_app_secret')->label( _t('Facebook App Secret', 'hbook') );
+		$ui->append('text', 'fb_scopes', 'option:hbook__fb_scopes')->label( _t('Facebook Scopes', 'hbook') );
 
-			$users = Users::get_by_info('facebook_id', $this->facebook->getUser());
-
-			if (isset($users[0])) {
-				$user = $users[0];
-
-				// Remember them
-				$user->remember();
-
-				// Store their token
-				$this->facebook->setExtendedAccessToken();
-				$user->info->hbook__facebook_token = $this->facebook->getAccessToken();
-				$user->update();
-			}
-		}
+		$ui->append( 'submit', 'save', _t( 'Save' ) );
+		$ui->out();
 	}
 
+	/**
+	 * Test whether we're ready to offer authentication
+	 */
 	private function is_ready()
 	{
 		if (Options::get('hbook__fb_app_id') && Options::get('hbook__fb_app_secret')) {
@@ -261,41 +188,85 @@ class Hbook extends Plugin
 		}
 	}
 
-	public function action_theme_loginform_controls()
-	{
-		Stack::add( 'admin_footer_javascript', URL::get_from_filesystem(__FILE__) . '/facebook.js', 'facebook', array('jquery') );
-
-		$button = '<button class="login facebook" data-fb-login="'. Options::get('hbook__fb_app_id') . '">Log in with Facebook</button>';
-		$button = Plugins::filter('login_button_facebook', $button, Options::get('hbook__fb_app_id'));
-
-		echo $button;
-	}
-
 	/**
-	 * Build main plugin configuration form
-	 * @return void
+	 * Build the authentication link
+	 * @param  string $type   Either 'oauth' or 'token'
+	 * @param  array $params  Optional parameters
+	 * @return string
 	 */
-	public function configure()
-	{
-		$ui = new FormUI( 'Hbook' );
-
-		$ui->append('text', 'fb_app_id', 'option:hbook__fb_app_id', _t('Facebook App ID', 'hbook'));
-		$ui->append('text', 'fb_app_secret', 'option:hbook__fb_app_secret', _t('Facebook App Secret', 'hbook'));
-
-		$ui->append( 'submit', 'save', _t( 'Save' ) );
-		$ui->out();
-	}
-
-	/**
-     * Add the configuration to the user page 
-     **/
-    public function action_form_user( $form, $user )
+	private function build_link($type, $params)
     {
+    	switch ($type) {
+    		case 'oauth':
+    			$url = "https://www.facebook.com/dialog/oauth?";
+    			break;
+    		case 'token':
+    			$url = 'https://graph.facebook.com/oauth/access_token?';
+    			break;
+    	}
 
-    	$form->user_info->append('text', 'facebook_id', $user, _t('Facebook User ID', 'hbook'), 'optionscontrol_text');
-    	$form->user_info->facebook_id->class[] = 'item';
-    	$form->user_info->facebook_id->class[] = 'clear';
-            
+    	$url .= 'client_id=' . Options::get('hbook__fb_app_id');
+
+    	if ($type == 'token') {
+    		$url .= '&client_secret=' . Options::get('hbook__fb_app_secret');
+    		$url .= '&code=' . $params['code'];
+    	}
+
+    	if ($type == 'oauth') {
+    		if (isset($params['scope'])) {
+	    		$scopes = $params['scope'];
+	    	} else {
+	    		$scopes = Options::get('hbook__fb_scopes');
+	    	}
+
+	    	if ($scopes !== null) {
+	    		if (is_string($scopes)) {
+	    			$scopes = str_replace(' ', '', $scopes);
+	    			$scopes = explode(',', $scopes);
+	    		}
+	    		$scopes = implode(',', $scopes);
+	    		$url.= '&scopes=' . $scopes;
+	    	}
+    	}
+
+		$url .= "&redirect_uri=" . URL::get('facebook_oauth_callback');
+
+		if(isset($params['state'])) {
+			$url .= "&state=" . $params['state'];
+		}
+
+		return $url;
+    }
+
+    /**
+     * Make an API request to Facebook
+     * @param  string $path  The path to request, including leading /
+     * @param  string $token The access_token to use
+     * @return object        The response, decoded
+     */
+    private function api( $path, $token )
+    {
+    	$base = 'https://graph.facebook.com';
+
+    	$request = new RemoteRequest($base . $path . '?access_token=' . $token , "GET");
+		$request->add_header('Accept: application/json');
+		$request->execute();
+
+		$response = $request->get_response_body();
+		$response = json_decode($response);
+
+		return $response;
+
+    }
+
+    /**
+     * Generate the profile URL for a given user
+     * @param  string $user_id The user to generate for
+     * @return string          The URL
+     */
+    private function get_portrait_url($user_id)
+    {
+    	return 'https://graph.facebook.com/' . $user_id . '/picture';
     }
 
 }
