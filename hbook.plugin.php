b4c0a1192a6b09921edc24bc79741a8409c7b98c
Morgante Pell <morgante.pell@morgante.net>
2014-01-12 23:48:59 +0400

    socialadmin compatibility

--- hbook.plugin.php
+++ hbook.plugin.php
@@ -12,23 +12,161 @@ use Facebook;
 class Hbook extends Plugin
 {
 	private $facebook;
+	private $service = 'facebook';
 
 	public function action_init()
 	{
 		if ($this->is_ready()) {
-			$this->facebook = new Facebook(array(
-				'appId' => Options::get('hbook__fb_app_id'),
-				'secret' => Options::get('hbook__fb_app_secret'),
-				'cookie' => true,
-			));
+			// $this->facebook = new Facebook(array(
+				// 'appId' => Options::get('hbook__fb_app_id'),
+				// 'secret' => Options::get('hbook__fb_app_secret'),
+				// 'cookie' => true,
+			// ));
 
 			$this->add_rule('"auth"/"facebook"/"add"', 'add_facebook_user');
+			$this->add_rule('"auth"/"facebook"/"callback"', 'facebook_oauth_callback');
 
-			$this->check_user();
+			// $this->check_user();
 
-			Stack::add( 'admin_header_javascript', URL::get_from_filesystem(__FILE__) . '/admin.js', 'hbook-admin', array('jquery') );
+			// Stack::add( 'admin_header_javascript', URL::get_from_filesystem(__FILE__) . '/admin.js', 'hbook-admin', array('jquery') );
 		}
 	}
+
+	/*
+     * Add Facebook to the list of social services providing the socialauth feature
+     */
+    public function filter_socialauth_services($services = array())
+    {
+    	if ( $this->is_ready() ) {
+    		$services[] = $this->service;
+    	}
+    	return $services;
+    }
+
+    /*
+     * Provide auth link to the theme
+     * @param string $service The service / social network the link is requested for.
+     * @param array Accepts values for overriding the global options redirect_uri and scope and additional state
+     */
+    public function theme_socialauth_link($theme, $service, $params = array())
+    {
+		if($service == $this->service) {
+			return $this->build_link('oauth', $params);
+		}
+    }
+
+    private function build_link($type, $params)
+    {
+    	switch ($type) {
+    		case 'oauth':
+    			$url = "https://www.facebook.com/dialog/oauth?";
+    			break;
+    		case 'token':
+    			$url = 'https://graph.facebook.com/oauth/access_token?';
+    			break;
+    	}
+
+    	$url .= 'client_id=' . Options::get('hbook__fb_app_id');
+
+    	if ($type == 'token') {
+    		$url .= '&client_secret=' . Options::get('hbook__fb_app_secret');
+    		$url .= '&code=' . $params['code'];
+    	}
+
+    	if ($type == 'oauth') {
+    		if (isset($params['scope'])) {
+	    		$scopes = $params['scope'];
+	    	} else {
+	    		$scopes = Options::get('hbook__fb_scopes');
+	    	}
+
+	    	if ($scopes !== null) {
+	    		if (is_string($scopes)) {
+	    			$scopes = str_replace(' ', '', $scopes);
+	    			$scopes = explode(',', $scopes);
+	    		}
+	    		$scopes = implode(',', $scopes);
+	    		$url.= '&scopes=' . $scopes;
+	    	}
+    	}
+
+		$url .= "&redirect_uri=" . URL::get('facebook_oauth_callback');
+
+		if(isset($params['state'])) {
+			$url .= "&state=" . $params['state'];
+		}
+
+		return $url;
+    }
+
+    private function api( $path, $token )
+    {
+    	$base = 'https://graph.facebook.com';
+
+    	$request = new RemoteRequest($base . $path . '?access_token=' . $token , "GET");
+		$request->add_header('Accept: application/json');
+		$request->execute();
+
+		$response = $request->get_response_body();
+		$response = json_decode($response);
+
+		return $response;
+
+    }
+
+    private function get_portrait_url($user_id)
+    {
+    	return 'https://graph.facebook.com/' . $user_id . '/picture';
+    }
+
+    public function action_plugin_act_facebook_oauth_callback($handler)
+    {
+    	$code = $_GET['code'];
+		$state =$_GET['state'];
+		$opts = Options::get_group(__CLASS__);
+
+		// Wrap in try/catch so we fail silently if user info not available
+		try {
+			/// Exchange code for token
+			$request = new RemoteRequest($this->build_link( 'token', array( 'code' => $code )), "POST");
+			$request->execute();
+
+			if ( ! $request->executed() ) {
+				throw new XMLRPCException( 16 );
+			}
+
+			parse_str($request->get_response_body(), $response);
+
+			$token = $response['access_token'];
+			
+			// Offer the token to plugins that want to do something with the authenticated user
+			Plugins::act('facebook_token', $token);
+
+			$data = $this->api( '/me', $token);
+
+			// The following is important, because it's part of the "socialauth" feature API
+			$user = array("id" => $data->id);
+			
+			if(isset($data->name)) {
+				$user['name'] = $data->name;
+			} else {
+				$user['name'] = $data->id;
+			}
+
+			$user['portrait_url'] = $this->get_portrait_url($data->id);
+
+			if(isset($data->email) && !empty($data->email)) {
+				$user['email'] = $data->email;
+			} else {
+				$user['email'] = $data->id . '@facebook.com';
+			}
+
+			// Pass the identification data to plugins
+			Plugins::act('socialauth_identified', $this->service, $user, $state);
+		} catch(Exception $e) {
+			// don't care if it fails, the only consequence is that action_social_auth will not be triggered, which is correct
+		}
+    }
 
 	public function action_plugin_act_add_facebook_user()
 	{
